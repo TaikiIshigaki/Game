426a9510d333a3c89d7b3c4c8ae920f01145d9a3
TaikiIshigaki <38103135+TaikiIshigaki@users.noreply.github.com>
2018-04-05 21:01:10 +0900

    Add files via upload

---
+++ Clear/ToTitle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ToTitle : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F))//RでMainSceneをロード
+        {
+            SceneManager.LoadScene("Title");
+        }
+    }
+}
---
+++ Gameover/Gameover.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Gameover : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R))//RでMainSceneをロード
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+
+        if (Input.GetKey(KeyCode.F))//RでMainSceneをロード
+        {
+            SceneManager.LoadScene("Title");
+        }
+    }
+}
